717ead7dc2780f87c86bfed36766b1aa46aa27e8
Rick van der Wolk <rickvanderwolk@gmail.com>
2015-11-15 19:46:28 +0100

    first draft

---
+++ Validate.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace Prototypeblocks;
+
+class Validate {
+
+    /** 
+    * Check if given variable type is array.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isArray($input = false) {
+
+        if ($input && is_array($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given variable type is boolean.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isBool($input = false) {
+
+        return self::isBoolean($input);
+    }
+
+    /** 
+    * Check if given variable type is boolean.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isBoolean($input = false) {
+
+        if ($input && is_bool($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if input is a valid date. 
+    * @param string input 
+    * @param string format 
+    * @return boolean
+    */ 
+    public static function isDate($input = false, $format = 'Y-m-d H:i:s') {
+
+        if($input) {
+
+            $DateTime = DateTime::createFromFormat($format, $input);
+            return $DateTime && $DateTime->format($format) == $input;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is a valid e-mail address.   
+    * @param string input 
+    * @return boolean
+    */ 
+    public static function isEmail($input = false) {
+
+        if ($input && !filter_var($input, FILTER_VALIDATE_EMAIL) === false) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given variable type is float.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isFloat($input = false) {
+
+        if ($input && is_float($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given variable type is integer.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isInt($input = false) {
+
+        return self::isInteger($input);
+    }
+
+    /** 
+    * Check if given variable type is integer.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isInteger($input = false) {
+
+        if ($input && is_int($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is a valid IP address.   
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isIp($input = false) {
+
+        if ($input && !filter_var($input, FILTER_VALIDATE_IP) === false) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is valid JSON. 
+    * @param string input 
+    * @return boolean
+    */ 
+    public static function isJson($input = false) {
+
+        if ($input && is_object(json_decode($input))) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check the length of a string. 
+    * @param string input 
+    * @param int min value 
+    * @param int max value 
+    * @example isLength('hello world', 2) - returns true, 9 is longer then 2
+    * @example isLength('hello world', 2, 5) - returns false, 9 is longer then 5
+    * @return boolean
+    */ 
+    public static function isLength($string = false, $minLength = 0, $maxLength = false) {
+
+        if ($string && strlen($string) >= $minLength) {
+
+            if (!$maxLength || strlen($string) <= $maxLength) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is a valid MAC address.   
+    * @param string input 
+    * @return boolean
+    */ 
+    public static function isMac($input = false) {
+
+        if ($input && !filter_var($input, FILTER_VALIDATE_MAC) === false) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is numeric.   
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isNumber($input = false) {
+
+        return self::isNumeric($input);
+    }
+
+    /** 
+    * Check if given input is numeric.   
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isNumeric($input = false) {
+
+        if ($input && is_numeric($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is an object.  
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isObject($input = false) {
+
+        if ($input && is_object($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is a string. 
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isString($input = false) {
+
+        if ($input && is_string($input)) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /** 
+    * Check if given input is a valid URL. 
+    * @param mixed input 
+    * @return boolean
+    */ 
+    public static function isUrl($input = false) {
+
+        if ($input && !filter_var($input, FILTER_VALIDATE_URL) === false) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
